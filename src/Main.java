b31211cf73f2a2a5a69b9f9e0359c60706c13b66
Kroissan <66138267+Kroissan@users.noreply.github.com>
2021-10-27 12:04:46 +0200

    rollback

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import jeu.*;
 public class Main {
+
     public static void main(String[] args){
+        System.out.println(getRandomCode("easy"));
+    }
+
+    public static Ligne getRandomCode(String diff){
+        int dif = Constantes.difficulty.get(diff);
+        Pion[] res = new Pion[5];
+        for(int i=0;i<5;i++){
+            res[i] = new Pion(Constantes.colors[(int)(Math.random()*dif)]);
+        }
+        return new Ligne(res);
     }
 }

bf8077c447b8de7c022daebb6e66dc99a9a23501
NathanKr <nathankr@orange.fr>
2021-10-27 10:13:26 +0200

    add constantes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+
     public static void main(String[] args){
+        System.out.println(getRandomCode(4));
+    }
+
+    public static Ligne getRandomCode(int size, int diff){
+        Pion[] res = new Pion[size];
+        for(int i=0;i<size;i++){
+            res[i] = new Pion(Constantes.colors[0]);
+        }
+        return new Ligne(res);
     }
 }

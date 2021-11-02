b93e719021e2cb3f5e03e39508fbb954bebf90fc
NathanKr <nathankr@orange.fr>
2021-11-02 11:47:07 +0100

    Constantes update

--- src/jeu/Constantes.java
+++ src/jeu/Constantes.java
@@ -7,9 +7,27 @@ import static java.awt.Color.*;
 
 public final class Constantes {
     public final static Color[] colors = new Color[]{red, blue, green, white, black, cyan, orange, magenta};
-    public final static HashMap<String, Integer> difficulty = new HashMap<String, Integer>(){{
-        put("easy", Integer.valueOf(4));
-        put("normal", Integer.valueOf(6));
-        put("hard", Integer.valueOf(8));
+    public final static HashMap<String, Integer> colorNumber = new HashMap<>() {{
+        put("easy", 6);
+        put("normal", 7);
+        put("hard", 8);
+    }};
+
+    public final static HashMap<String, Integer> rowSize = new HashMap<>() {{
+        put("easy", 4);
+        put("normal", 5);
+        put("hard", 6);
+    }};
+
+    public final static HashMap<String, Integer> numberRow = new HashMap<>() {{
+        put("easy", 14);
+        put("normal", 12);
+        put("hard", 10);
+    }};
+
+    public final static HashMap<String, Boolean> remise = new HashMap<>() {{
+        put("easy", Boolean.FALSE);
+        put("normal", Boolean.FALSE);
+        put("hard", Boolean.TRUE);
     }};
 }

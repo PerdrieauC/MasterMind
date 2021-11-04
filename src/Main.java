dc0a668d7e37f68b5ba73305c6fa40cf4efc3fa2
NathanKr <nathankr@orange.fr>
2021-11-04 09:20:51 +0100

    testing classes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import jeu.*;
-
+import controller.*;
 import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args){
         Plateau mastermind = new Plateau("easy");
-        System.out.println(new Pion(Constantes.colors[0]).getCouleur()==new Pion(Constantes.colors[0]).getCouleur());
+        mastermind.inputLigne(game.getRandomCode(4,6,false));
+        mastermind.inputLigne(game.getRandomCode(4,6,false));
+        mastermind.inputLigne(game.getRandomCode(4,6,false));
+        System.out.println(mastermind);
     }
 
 }
